908a45e7cbdb8edd1eb82be488f95307f0947ed0
FaniZuhri <fanizuhri16@gmail.com>
2025-05-01 22:22:33 +0700

    add initial working version of lpuart

--- Core/Inc/usart.h
+++ Core/Inc/usart.h
@@ -29,16 +29,36 @@ extern "C" {
 #include "main.h"
 
 /* USER CODE BEGIN Includes */
+#define LPUART_RX_DATA_SIZE		256U
+#define LPUART_TX_DATA_SIZE		128U
+#define UART_DBG_TX_DATA_SIZE	128U
 
+typedef enum {
+	UART_SCP_DMA_TX_UNCOMPLETE,
+	UART_SCP_DMA_TX_COMPLETE,
+} uart_scp_dma_status_t;
 /* USER CODE END Includes */
 
 /* USER CODE BEGIN Private defines */
-
+extern uint8_t uart_tx_buf[UART_DBG_TX_DATA_SIZE], uart_start_byte_idx;
+extern uint8_t lpuart_rx_buf[LPUART_RX_DATA_SIZE], lpuart_tx_buf[LPUART_TX_DATA_SIZE];
+extern volatile uart_scp_dma_status_t lpuart_scp_tx_status;
 /* USER CODE END Private defines */
 
 void MX_LPUART1_UART_Init(void);
+void MX_USART1_UART_Init(void);
 
 /* USER CODE BEGIN Prototypes */
+__STATIC_INLINE uint8_t lpuart_get_rx_buf(uint8_t idx) {
+	return lpuart_rx_buf[idx];
+}
+
+__STATIC_INLINE void lpuart_set_rx_buf(uint8_t idx, uint8_t data) {
+	lpuart_rx_buf[idx] = data;
+}
+
+void lpuart_dma_transmit(uint8_t *tx_buf, uint8_t tx_size);
+void lpuart_dma_receive(void);
 
 /* USER CODE END Prototypes */
 
